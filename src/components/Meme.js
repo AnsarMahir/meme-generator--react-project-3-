3c2add8bdf86e9a190fbdcec5fa5d5a10ba3e658
Ansak Mahir <ansarmmm.21@uom.lk>
2024-10-24 21:30:11 +0530

    random rul fetched

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,25 @@
 import React from "react";
+import memeData from "../memeData";
 
 export default function Meme(){
+    let url;
+    function handleClick(){
+        let memesArray = memeData.data.memes;
+        let number =  Math.floor(Math.random() * memesArray.length);
+        console.log(url = memeData.data.memes[number].url);
+    }
+
+    function hanleMouseOver(){
+        console.log("Mouseover")
+    }
     return(
-        <form className="meme--form">
-            <div className="meme--inputs">
-                <label for="top text">top text</label>
-                <input type="text" name="top"></input>
-                <label for="bottom text">bottom text</label>
-                <input type="text" name="bottom"></input>
-            </div>
-            <button type="submit" className="meme--button">Get a new image</button>
-        </form>
+        <main>
+        <p>{url}</p>
+        <div className="meme--form">
+            <input type="text" name="top" className="meme--input" placeholder="Top text"></input>
+            <input type="text" name="bottom" className="meme--input" placeholder="Bottom text"></input>
+            <button type="submit" onClick={handleClick} className="meme--button">Get a new image</button>
+        </div>
+        </main>
     )
 }
